026124c181512e51b12d13998bd526aec0c4e43e
Szymon Piechaczek <piechaczek.szymon@gmail.com>
2017-04-15 16:28:08 +0200

    First personalizing

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,40 @@ var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
 
 var sassPaths = [
-  'bower_components/normalize.scss/sass',
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
+    'bower_components/foundation-sites/scss',
+    'bower_components/motion-ui/src'
 ];
 
 gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
+    return gulp.src('scss/app.scss')
+        .pipe($.sass({
+            includePaths: sassPaths,
+            outputStyle: 'compressed' // if css compressed **file size**
+        })
+            .on('error', $.sass.logError))
+        .pipe($.autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9']
+        }))
+        .pipe(gulp.dest('css'));
 });
 
 gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+    gulp.watch(['scss/**/*.scss'], ['sass']);
+});
+
+var js_vendors = [
+    'bower_components/jquery/dist/jquery.min.js',
+    // 'bower_components/slick-carousel/slick/slick.min.js',
+    'bower_components/what-input/what-input.js',
+    'bower_components/foundation-sites/dist/js/foundation.js'
+];
+
+gulp.task('setup-vendor', function(){
+    return gulp.src(js_vendors)
+        .pipe(gulp.dest('./js'))
+        .pipe($.concat('vendor.js'))
+        .pipe(gulp.dest('./js'))
+        .pipe($.rename('vendor.min.js'))
+        .pipe($.uglify())
+        .pipe(gulp.dest('./js'))
 });
